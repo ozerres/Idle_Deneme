7dc57d225fe4641125964d1f5d38414b6f3ad2f9
ozerres <eskimezozer@gmail.com>
2023-09-14 10:39:26 +0300

    UI changes

---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+
+    int StoreCount;
+
+    
+    void Start()
+    {
+
+        StoreCount - 1;
+
+    }
+
+    
+    void Update()
+    {
+
+
+
+    }
+
+    void BuyStoreOnClick ()
+    {
+
+        StoreCount - StoreCount + 1;
+
+    }
+}
